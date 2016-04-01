0a20e8649523d7f9b4a1362714cfb3da4591dbea
v2kovac <v2kovac@uwaterloo.ca>
2016-04-01 16:59:14 -0400

    p2b

---
+++ pii/g32_myproject/emit/ecelinux5.uwaterloo.ca/config/a11cab2c947dc5b1695ec95a3df223f2/g++-config-0/coverity-macro-compat.h
@@ -0,0 +1,304 @@
+﻿
+#if !(defined(__DBL_MIN_EXP__) || defined(__coverity_undefine___DBL_MIN_EXP__))
+#define __DBL_MIN_EXP__ (-1021)
+#endif
+#if !(defined(__FLT_MIN__) || defined(__coverity_undefine___FLT_MIN__))
+#define __FLT_MIN__ 1.17549435e-38F
+#endif
+#if !(defined(__CHAR_BIT__) || defined(__coverity_undefine___CHAR_BIT__))
+#define __CHAR_BIT__ 8
+#endif
+#if !(defined(__WCHAR_MAX__) || defined(__coverity_undefine___WCHAR_MAX__))
+#define __WCHAR_MAX__ 2147483647
+#endif
+#if !(defined(__DBL_DENORM_MIN__) || defined(__coverity_undefine___DBL_DENORM_MIN__))
+#define __DBL_DENORM_MIN__ 4.9406564584124654e-324
+#endif
+#if !(defined(__FLT_EVAL_METHOD__) || defined(__coverity_undefine___FLT_EVAL_METHOD__))
+#define __FLT_EVAL_METHOD__ 0
+#endif
+#if !(defined(__DBL_MIN_10_EXP__) || defined(__coverity_undefine___DBL_MIN_10_EXP__))
+#define __DBL_MIN_10_EXP__ (-307)
+#endif
+#if !(defined(__FINITE_MATH_ONLY__) || defined(__coverity_undefine___FINITE_MATH_ONLY__))
+#define __FINITE_MATH_ONLY__ 0
+#endif
+#if !(defined(__LP64__) || defined(__coverity_undefine___LP64__))
+#define __LP64__ 1
+#endif
+#if !(defined(__SHRT_MAX__) || defined(__coverity_undefine___SHRT_MAX__))
+#define __SHRT_MAX__ 32767
+#endif
+#if !(defined(__LDBL_MAX__) || defined(__coverity_undefine___LDBL_MAX__))
+#define __LDBL_MAX__ 1.18973149535723176502e+4932L
+#endif
+#if !(defined(__UINTMAX_TYPE__) || defined(__coverity_undefine___UINTMAX_TYPE__))
+#define __UINTMAX_TYPE__ long unsigned int
+#endif
+#if !(defined(__linux) || defined(__coverity_undefine___linux))
+#define __linux 1
+#endif
+#if !(defined(__unix) || defined(__coverity_undefine___unix))
+#define __unix 1
+#endif
+#if !(defined(__linux__) || defined(__coverity_undefine___linux__))
+#define __linux__ 1
+#endif
+#if !(defined(__SCHAR_MAX__) || defined(__coverity_undefine___SCHAR_MAX__))
+#define __SCHAR_MAX__ 127
+#endif
+#if !(defined(__USER_LABEL_PREFIX__) || defined(__coverity_undefine___USER_LABEL_PREFIX__))
+#define __USER_LABEL_PREFIX__ 
+#endif
+#if !(defined(__STDC_HOSTED__) || defined(__coverity_undefine___STDC_HOSTED__))
+#define __STDC_HOSTED__ 1
+#endif
+#if !(defined(__DBL_DIG__) || defined(__coverity_undefine___DBL_DIG__))
+#define __DBL_DIG__ 15
+#endif
+#if !(defined(__FLT_EPSILON__) || defined(__coverity_undefine___FLT_EPSILON__))
+#define __FLT_EPSILON__ 1.19209290e-7F
+#endif
+#if !(defined(__GXX_WEAK__) || defined(__coverity_undefine___GXX_WEAK__))
+#define __GXX_WEAK__ 1
+#endif
+#if !(defined(__LDBL_MIN__) || defined(__coverity_undefine___LDBL_MIN__))
+#define __LDBL_MIN__ 3.36210314311209350626e-4932L
+#endif
+#if !(defined(__unix__) || defined(__coverity_undefine___unix__))
+#define __unix__ 1
+#endif
+#if !(defined(__DECIMAL_DIG__) || defined(__coverity_undefine___DECIMAL_DIG__))
+#define __DECIMAL_DIG__ 21
+#endif
+#if !(defined(__gnu_linux__) || defined(__coverity_undefine___gnu_linux__))
+#define __gnu_linux__ 1
+#endif
+#if !(defined(__LDBL_HAS_QUIET_NAN__) || defined(__coverity_undefine___LDBL_HAS_QUIET_NAN__))
+#define __LDBL_HAS_QUIET_NAN__ 1
+#endif
+#if !(defined(__GNUC__) || defined(__coverity_undefine___GNUC__))
+#define __GNUC__ 4
+#endif
+#if !(defined(__MMX__) || defined(__coverity_undefine___MMX__))
+#define __MMX__ 1
+#endif
+#if !(defined(__DBL_MAX__) || defined(__coverity_undefine___DBL_MAX__))
+#define __DBL_MAX__ 1.7976931348623157e+308
+#endif
+#if !(defined(__DBL_HAS_INFINITY__) || defined(__coverity_undefine___DBL_HAS_INFINITY__))
+#define __DBL_HAS_INFINITY__ 1
+#endif
+#if !(defined(__cplusplus) || defined(__coverity_undefine___cplusplus))
+#define __cplusplus 1
+#endif
+#if !(defined(__DEPRECATED) || defined(__coverity_undefine___DEPRECATED))
+#define __DEPRECATED 1
+#endif
+#if !(defined(__DBL_MAX_EXP__) || defined(__coverity_undefine___DBL_MAX_EXP__))
+#define __DBL_MAX_EXP__ 1024
+#endif
+#if !(defined(__SSE2_MATH__) || defined(__coverity_undefine___SSE2_MATH__))
+#define __SSE2_MATH__ 1
+#endif
+#if !(defined(__amd64) || defined(__coverity_undefine___amd64))
+#define __amd64 1
+#endif
+#if !(defined(__GNUG__) || defined(__coverity_undefine___GNUG__))
+#define __GNUG__ 4
+#endif
+#if !(defined(__LONG_LONG_MAX__) || defined(__coverity_undefine___LONG_LONG_MAX__))
+#define __LONG_LONG_MAX__ 9223372036854775807LL
+#endif
+#if !(defined(__GXX_ABI_VERSION) || defined(__coverity_undefine___GXX_ABI_VERSION))
+#define __GXX_ABI_VERSION 1002
+#endif
+#if !(defined(__FLT_MIN_EXP__) || defined(__coverity_undefine___FLT_MIN_EXP__))
+#define __FLT_MIN_EXP__ (-125)
+#endif
+#if !(defined(__x86_64) || defined(__coverity_undefine___x86_64))
+#define __x86_64 1
+#endif
+#if !(defined(__DBL_MIN__) || defined(__coverity_undefine___DBL_MIN__))
+#define __DBL_MIN__ 2.2250738585072014e-308
+#endif
+#if !(defined(__FLT_MIN_10_EXP__) || defined(__coverity_undefine___FLT_MIN_10_EXP__))
+#define __FLT_MIN_10_EXP__ (-37)
+#endif
+#if !(defined(__DBL_HAS_QUIET_NAN__) || defined(__coverity_undefine___DBL_HAS_QUIET_NAN__))
+#define __DBL_HAS_QUIET_NAN__ 1
+#endif
+#if !(defined(__REGISTER_PREFIX__) || defined(__coverity_undefine___REGISTER_PREFIX__))
+#define __REGISTER_PREFIX__ 
+#endif
+#if !(defined(__FLT_MANT_DIG__) || defined(__coverity_undefine___FLT_MANT_DIG__))
+#define __FLT_MANT_DIG__ 24
+#endif
+#if !(defined(__VERSION__) || defined(__coverity_undefine___VERSION__))
+#define __VERSION__ "4.1.2 20080704 (Red Hat 4.1.2-55)"
+#endif
+#if !(defined(unix) || defined(__coverity_undefine_unix))
+#define unix 1
+#endif
+#if !(defined(__SIZE_TYPE__) || defined(__coverity_undefine___SIZE_TYPE__))
+#define __SIZE_TYPE__ long unsigned int
+#endif
+#if !(defined(__ELF__) || defined(__coverity_undefine___ELF__))
+#define __ELF__ 1
+#endif
+#if !(defined(__FLT_RADIX__) || defined(__coverity_undefine___FLT_RADIX__))
+#define __FLT_RADIX__ 2
+#endif
+#if !(defined(__LDBL_EPSILON__) || defined(__coverity_undefine___LDBL_EPSILON__))
+#define __LDBL_EPSILON__ 1.08420217248550443401e-19L
+#endif
+#if !(defined(__GNUC_RH_RELEASE__) || defined(__coverity_undefine___GNUC_RH_RELEASE__))
+#define __GNUC_RH_RELEASE__ 55
+#endif
+#if !(defined(__k8) || defined(__coverity_undefine___k8))
+#define __k8 1
+#endif
+#if !(defined(__LDBL_DIG__) || defined(__coverity_undefine___LDBL_DIG__))
+#define __LDBL_DIG__ 18
+#endif
+#if !(defined(__x86_64__) || defined(__coverity_undefine___x86_64__))
+#define __x86_64__ 1
+#endif
+#if !(defined(__FLT_HAS_QUIET_NAN__) || defined(__coverity_undefine___FLT_HAS_QUIET_NAN__))
+#define __FLT_HAS_QUIET_NAN__ 1
+#endif
+#if !(defined(__FLT_MAX_10_EXP__) || defined(__coverity_undefine___FLT_MAX_10_EXP__))
+#define __FLT_MAX_10_EXP__ 38
+#endif
+#if !(defined(__LONG_MAX__) || defined(__coverity_undefine___LONG_MAX__))
+#define __LONG_MAX__ 9223372036854775807L
+#endif
+#if !(defined(__FLT_HAS_INFINITY__) || defined(__coverity_undefine___FLT_HAS_INFINITY__))
+#define __FLT_HAS_INFINITY__ 1
+#endif
+#if !(defined(linux) || defined(__coverity_undefine_linux))
+#define linux 1
+#endif
+#if !(defined(__EXCEPTIONS) || defined(__coverity_undefine___EXCEPTIONS))
+#define __EXCEPTIONS 1
+#endif
+#if !(defined(__LDBL_MANT_DIG__) || defined(__coverity_undefine___LDBL_MANT_DIG__))
+#define __LDBL_MANT_DIG__ 64
+#endif
+#if !(defined(__k8__) || defined(__coverity_undefine___k8__))
+#define __k8__ 1
+#endif
+#if !(defined(__WCHAR_TYPE__) || defined(__coverity_undefine___WCHAR_TYPE__))
+#define __WCHAR_TYPE__ int
+#endif
+#if !(defined(__FLT_DIG__) || defined(__coverity_undefine___FLT_DIG__))
+#define __FLT_DIG__ 6
+#endif
+#if !(defined(__INT_MAX__) || defined(__coverity_undefine___INT_MAX__))
+#define __INT_MAX__ 2147483647
+#endif
+#if !(defined(__FLT_MAX_EXP__) || defined(__coverity_undefine___FLT_MAX_EXP__))
+#define __FLT_MAX_EXP__ 128
+#endif
+#if !(defined(__DBL_MANT_DIG__) || defined(__coverity_undefine___DBL_MANT_DIG__))
+#define __DBL_MANT_DIG__ 53
+#endif
+#if !(defined(__WINT_TYPE__) || defined(__coverity_undefine___WINT_TYPE__))
+#define __WINT_TYPE__ unsigned int
+#endif
+#if !(defined(__SSE__) || defined(__coverity_undefine___SSE__))
+#define __SSE__ 1
+#endif
+#if !(defined(__LDBL_MIN_EXP__) || defined(__coverity_undefine___LDBL_MIN_EXP__))
+#define __LDBL_MIN_EXP__ (-16381)
+#endif
+#if !(defined(__amd64__) || defined(__coverity_undefine___amd64__))
+#define __amd64__ 1
+#endif
+#if !(defined(__LDBL_MAX_EXP__) || defined(__coverity_undefine___LDBL_MAX_EXP__))
+#define __LDBL_MAX_EXP__ 16384
+#endif
+#if !(defined(__LDBL_MAX_10_EXP__) || defined(__coverity_undefine___LDBL_MAX_10_EXP__))
+#define __LDBL_MAX_10_EXP__ 4932
+#endif
+#if !(defined(__DBL_EPSILON__) || defined(__coverity_undefine___DBL_EPSILON__))
+#define __DBL_EPSILON__ 2.2204460492503131e-16
+#endif
+#if !(defined(_LP64) || defined(__coverity_undefine__LP64))
+#define _LP64 1
+#endif
+#if !(defined(__GNUC_PATCHLEVEL__) || defined(__coverity_undefine___GNUC_PATCHLEVEL__))
+#define __GNUC_PATCHLEVEL__ 2
+#endif
+#if !(defined(__LDBL_HAS_INFINITY__) || defined(__coverity_undefine___LDBL_HAS_INFINITY__))
+#define __LDBL_HAS_INFINITY__ 1
+#endif
+#if !(defined(__INTMAX_MAX__) || defined(__coverity_undefine___INTMAX_MAX__))
+#define __INTMAX_MAX__ 9223372036854775807L
+#endif
+#if !(defined(__FLT_DENORM_MIN__) || defined(__coverity_undefine___FLT_DENORM_MIN__))
+#define __FLT_DENORM_MIN__ 1.40129846e-45F
+#endif
+#if !(defined(__FLT_MAX__) || defined(__coverity_undefine___FLT_MAX__))
+#define __FLT_MAX__ 3.40282347e+38F
+#endif
+#if !(defined(__SSE2__) || defined(__coverity_undefine___SSE2__))
+#define __SSE2__ 1
+#endif
+#if !(defined(__INTMAX_TYPE__) || defined(__coverity_undefine___INTMAX_TYPE__))
+#define __INTMAX_TYPE__ long int
+#endif
+#if !(defined(__GNUC_MINOR__) || defined(__coverity_undefine___GNUC_MINOR__))
+#define __GNUC_MINOR__ 1
+#endif
+#if !(defined(__DBL_MAX_10_EXP__) || defined(__coverity_undefine___DBL_MAX_10_EXP__))
+#define __DBL_MAX_10_EXP__ 308
+#endif
+#if !(defined(__LDBL_DENORM_MIN__) || defined(__coverity_undefine___LDBL_DENORM_MIN__))
+#define __LDBL_DENORM_MIN__ 3.64519953188247460253e-4951L
+#endif
+#if !(defined(__STDC__) || defined(__coverity_undefine___STDC__))
+#define __STDC__ 1
+#endif
+#if !(defined(__PTRDIFF_TYPE__) || defined(__coverity_undefine___PTRDIFF_TYPE__))
+#define __PTRDIFF_TYPE__ long int
+#endif
+#if !(defined(__LDBL_MIN_10_EXP__) || defined(__coverity_undefine___LDBL_MIN_10_EXP__))
+#define __LDBL_MIN_10_EXP__ (-4931)
+#endif
+#if !(defined(__SSE_MATH__) || defined(__coverity_undefine___SSE_MATH__))
+#define __SSE_MATH__ 1
+#endif
+#if !(defined(__GNUC_GNU_INLINE__) || defined(__coverity_undefine___GNUC_GNU_INLINE__))
+#define __GNUC_GNU_INLINE__ 1
+#endif
+#if !(defined(_GNU_SOURCE) || defined(__coverity_undefine__GNU_SOURCE))
+#define _GNU_SOURCE 1
+#endif
+#if !(defined(__EXCEPTIONS) || defined(__coverity_undefine___EXCEPTIONS))
+#define __EXCEPTIONS 1
+#endif
+#if !(defined(__PTRDIFF_TYPE__) || defined(__coverity_undefine___PTRDIFF_TYPE__))
+#define __PTRDIFF_TYPE__ long int
+#endif
+#if !(defined(__REGISTER_PREFIX__) || defined(__coverity_undefine___REGISTER_PREFIX__))
+#define __REGISTER_PREFIX__ 
+#endif
+#if !(defined(__SIZE_TYPE__) || defined(__coverity_undefine___SIZE_TYPE__))
+#define __SIZE_TYPE__ long unsigned int
+#endif
+#if !(defined(__STDC__) || defined(__coverity_undefine___STDC__))
+#define __STDC__ 1
+#endif
+#if !(defined(__USER_LABEL_PREFIX__) || defined(__coverity_undefine___USER_LABEL_PREFIX__))
+#define __USER_LABEL_PREFIX__ 
+#endif
+#if !(defined(__VERSION__) || defined(__coverity_undefine___VERSION__))
+#define __VERSION__ "4.1.2 20080704 (Red Hat 4.1.2-55)"
+#endif
+#if !(defined(__WCHAR_TYPE__) || defined(__coverity_undefine___WCHAR_TYPE__))
+#define __WCHAR_TYPE__ int
+#endif
+#if !(defined(__cplusplus) || defined(__coverity_undefine___cplusplus))
+#define __cplusplus 1
+#endif
